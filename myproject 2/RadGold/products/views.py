78fa0585bfe53d7a03db0ab8a7963dd98990a895
nasrin <nasrinmohammadpur7611@gmail.com>
2021-06-23 22:48:08 +0430

    tages v1

--- myproject 2/RadGold/products/views.py
+++ myproject 2/RadGold/products/views.py
@@ -6,12 +6,44 @@ from .models import Post, Comment
 #from .forms import ImageForm
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from taggit.models import Tag
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-def post_list(request):
+
+
+
+
+
+def post_list(request, tag_slug=None):
     posts_all = Post.objects.filter(status='published')
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
+    object_list = Post.objects.filter(status='published')
+    paginator = Paginator(object_list,2) # 3 posts in each page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)    
+        
     return render(request,
-    'products/post/list.html',
-    {'posts': posts_all})
+        'products/post/list.html',
+            {'posts': posts_all,
+            'page': page,
+            'tag': tag})
+
+
+
+
+
+
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
